c476c602358e5a87d6ca16f8398c5901b9ebc24c
chadbrb1 <91875699+chadbrb1@users.noreply.github.com>
2023-01-25 10:23:17 +0100

    creation du projet

---
+++ src/main/java/com/example/nombreromain/NombreRomainApplication.java
@@ -0,0 +1,13 @@
+package com.example.nombreromain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NombreRomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NombreRomainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nombreromain/NombreRomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nombreromain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NombreRomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
