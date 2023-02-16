82a9847c19c80fab1a075d308f10d8ad3bdc84ac
chadbrb1 <chad.barbier@epsi.fr>
2023-02-16 14:04:17 +0100

    created method convertirChiffreArabeEnChiffreRomainAmeliore

--- src/main/java/com/example/nombreromain/NombreRomain.java
+++ src/main/java/com/example/nombreromain/NombreRomain.java
@@ -7,5 +7,19 @@ public class NombreRomain {
         return resultat;
     }
 
-    //public static String convertirChiffreArabeEnChiffre
+    public static String convertirChiffreArabeEnChiffreRomainAmeliore(int ChiffreArabe) {
+        String[] chiffreRomain = {"", "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] decimale = {0, 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String resultat = "";
+        int i = decimale.length - 1;
+        while (ChiffreArabe < 0){
+            int quotient = ChiffreArabe / decimale[i];
+            ChiffreArabe = ChiffreArabe % decimale[i];
+            while(quotient-- > 0){
+                resultat += chiffreRomain[i];
+            }
+            i--;
+        }
+        return resultat;
+    }
 }
